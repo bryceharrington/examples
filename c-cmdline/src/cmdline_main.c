13f65e9644725444d5fa6d84133ce083c3ee1a29
Andy Williams <andy@andywilliams.me>
2017-10-11 23:25:25 +0100

    Initial commit of a trivial EFL command line app

---
+++ c-cmdline/src/cmdline_main.c
@@ -0,0 +1,26 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <Eina.h>
+#include <Efl.h>
+#include <Elementary.h>
+
+EAPI_MAIN void
+efl_main(void *data EINA_UNUSED, const Efl_Event *ev)
+{
+   Efl_Loop_Arguments *args = ev->info;
+
+   if (eina_array_count(args->argv) == 0)
+     {
+        printf("Hello World!\n");
+     }
+   else
+     {
+        printf("Hello %s!\n", (char *) eina_array_data_get(args->argv, 0));
+     }
+
+   efl_exit();
+}
+EFL_MAIN()
+
