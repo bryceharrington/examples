e2d0cbf52a67ef96463e46ef0e3f0b1f8da6883b
Andy Williams <andy@andywilliams.me>
2017-11-03 17:20:33 +0000

    Add first pass Eo intro example

---
+++ c-eo-intro/src/eo_intro_main.c
@@ -0,0 +1,60 @@
+#define EFL_EO_API_SUPPORT 1
+#define EFL_BETA_API_SUPPORT 1
+
+#include <Eina.h>
+#include <Efl.h>
+#include <Elementary.h>
+
+#include <stdlib.h>
+
+Eo *_ref1, *_ref2;
+
+static Eo *
+_obj_create()
+{
+   Eo *obj, *child;
+   Eina_Bool finalized;
+
+   obj = efl_add(EFL_MODEL_ITEM_CLASS, NULL,
+                 efl_name_set(efl_added, "Parent Object"),
+                 finalized = efl_finalized_get(efl_added));
+   printf("Was the parent final? %d\n", finalized);
+   finalized = efl_finalized_get(obj);
+   printf("Is the parent final now? %d\n", finalized);
+
+   child = efl_add(EFL_MODEL_ITEM_CLASS, obj,
+                   efl_name_set(efl_added, "Child Object 1"));
+   efl_wref_add(child, &_ref1);
+   child = efl_add_ref(EFL_MODEL_ITEM_CLASS, obj,
+                       efl_name_set(efl_added, "Child Object 2"));
+   efl_wref_add(child, &_ref2);
+
+   return obj;
+}
+
+static void
+_obj_del(Eo *parent)
+{
+   printf("Child exists? %d, %d\n", _ref1!=NULL, _ref2!=NULL);
+
+   efl_del(parent);
+   printf("Child exists? %d, %d\n", _ref1!=NULL, _ref2!=NULL);
+
+   efl_del(_ref2);
+   printf("Child exists? %d, %d\n", _ref1!=NULL, _ref2!=NULL);
+}
+
+EAPI_MAIN void
+efl_main(void *data EINA_UNUSED, const Efl_Event *ev EINA_UNUSED)
+{
+   Eo* obj;
+
+   obj = _obj_create();
+   printf("Object name %s\n", efl_name_get(obj));
+
+   _obj_del(obj);
+
+   efl_exit(0);
+}
+EFL_MAIN()
+
