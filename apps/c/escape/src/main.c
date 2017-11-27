262afeb5a7cf6ebeba870963a995ff2e8ef86321
Bryce Harrington <bryce@osg.samsung.com>
2017-11-27 12:35:54 -0800

    escape: Draw screen using Cairo
    This demonstrates drawing a static image to the window's buffer
    by just brute force copying the pixels from the cairo buffer to the efl
    buffer.  (This could be improved on...)

--- apps/c/escape/src/main.c
+++ apps/c/escape/src/main.c
@@ -1,3 +1,6 @@
+#include <math.h>
+#include <cairo.h>
+
 #define EFL_EO_API_SUPPORT 1
 #define EFL_BETA_API_SUPPORT 1
 
@@ -6,71 +9,68 @@
 #include <Elementary.h>
 #include <Evas.h>
 
-static void
-_gui_editor_changed_cb(void *data, const Efl_Event *event EINA_UNUSED)
-{
-   Eo *editor = data;
+#define WIDTH 800
+#define HEIGHT 640
 
-   printf("Change recorded: %s\n", efl_text_get(editor));
-}
-
-static void
-_gui_about_clicked_cb(void *data, const Efl_Event *event EINA_UNUSED)
-{
-   Eo *button = data;
-
-   printf("Clicked About\n");
-}
-
-static void
-_gui_quit_clicked_cb(void *data EINA_UNUSED, const Efl_Event *event EINA_UNUSED)
-{
-   efl_exit(0);
-}
+static cairo_surface_t *surface;
+static cairo_t *cr;
 
 static void
 _gui_setup()
 {
-   Eo *win, *box, *hbox, *editor, *button;
+   Eo *win, *box;
+   Evas_Object *source_image;  // TODO: Should Evas_Object* really be an Eo*?
+   unsigned char *cairo_pixels;
+   unsigned char *efl_pixels;
 
    win = efl_add(EFL_UI_WIN_CLASS, NULL,
-                 efl_ui_win_type_set(efl_added, EFL_UI_WIN_BASIC),
-                 efl_text_set(efl_added, "Escape"),
-                 efl_ui_win_autodel_set(efl_added, EINA_TRUE));
+		 efl_ui_win_type_set(efl_added, EFL_UI_WIN_BASIC),
+		 efl_text_set(efl_added, "Escape"),
+		 efl_ui_win_autodel_set(efl_added, EINA_TRUE));
 
+   // Set default window size to our desired geometry
    box = efl_add(EFL_UI_BOX_CLASS, win,
-                efl_content_set(win, efl_added),
-                efl_gfx_size_hint_min_set(efl_added, EINA_SIZE2D(360, 240)));
+		efl_content_set(win, efl_added),
+		efl_gfx_size_hint_min_set(efl_added, EINA_SIZE2D(WIDTH, HEIGHT)));
 
-   // TODO: efl_ui_bg_class_get() to create a colorable background
+   source_image = efl_add(EFL_CANVAS_IMAGE_CLASS, box);
+   efl_gfx_buffer_copy_set(source_image, NULL, EINA_SIZE2D(WIDTH, HEIGHT), 0, EFL_GFX_COLORSPACE_ARGB8888, 0);
+   efl_gfx_position_set(source_image, EINA_POSITION2D(0, 0));
+   efl_gfx_size_set(source_image, EINA_SIZE2D((WIDTH),  (HEIGHT)));
+   efl_gfx_visible_set(source_image, EINA_TRUE);
 
-   // TODO: Turn this into a canvas
-   //  * Look at the efl_ API docs for any related to canvas
-   //  * I need something like evas_object_vg_add()
-   //  * ./lib/evas/canvas/efl_vg_gradient_linear.eo.h:typedef
-   editor = efl_add(EFL_UI_TEXT_CLASS, box,
-                    efl_text_set(efl_added, "Edit me"),
-                    efl_ui_text_interactive_editable_set(efl_added, EINA_TRUE),
-                    efl_ui_text_scrollable_set(efl_added, EINA_TRUE),
-                    efl_pack(box, efl_added));
-   efl_event_callback_add(editor, EFL_UI_TEXT_EVENT_CHANGED_USER,
-                          _gui_editor_changed_cb, editor);
+   surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, WIDTH, HEIGHT);
+   cr = cairo_create(surface);
+   cairo_set_source_rgb(cr, 1.0, 0.0, 0.0);
+   cairo_arc(cr, IMAGE_WIDTH / 2, IMAGE_HEIGHT / 2, IMAGE_WIDTH / 4.0, 0.0, 2.0 * M_PI);
+   cairo_fill(cr);
 
-   hbox = efl_add(EFL_UI_BOX_CLASS, box,
-                 efl_ui_direction_set(efl_added, EFL_UI_DIR_HORIZONTAL),
-                 efl_gfx_size_hint_weight_set(efl_added, 1.0, 0.1),
-                 efl_pack(box, efl_added));
-   button = efl_add(EFL_UI_BUTTON_CLASS, hbox,
-                    efl_text_set(efl_added, "About"),
-                    efl_pack(hbox, efl_added),
-                    efl_event_callback_add(efl_added, EFL_UI_EVENT_CLICKED,
-                                           _gui_about_clicked_cb, efl_added));
-   button = efl_add(EFL_UI_BUTTON_CLASS, hbox,
-                    efl_text_set(efl_added, "Quit"),
-                    efl_pack(hbox, efl_added),
-                    efl_event_callback_add(efl_added, EFL_UI_EVENT_CLICKED,
-                                           _gui_quit_clicked_cb, efl_added));
+   cairo_pixels = cairo_image_surface_get_data(surface);
+
+   int stride;
+   Eina_Rw_Slice slice;
+   slice = efl_gfx_buffer_map(source_image, EFL_GFX_BUFFER_ACCESS_MODE_WRITE, NULL,
+			      EFL_GFX_COLORSPACE_ARGB8888, 0, &stride);
+   efl_pixels = slice.mem;
+   if (!efl_pixels) return;
+
+   // Get pixels into source_image
+   // TODO: Isn't there a more direct way to do this?
+   int i, j;
+   for (i = 0; i<HEIGHT; i++) {
+      unsigned int *source_row = (unsigned int *)((char *)cairo_pixels + stride * i);
+      unsigned int *dest_row = (unsigned int *)((char *)efl_pixels + stride * i);
+
+      for (j = 0; j < WIDTH; j++) {
+	 // Copy cairo_pixels to efl_pixels
+	 *dest_row++ = (*source_row++) | 0xff000000;
+      }
+   }
+
+   efl_gfx_buffer_unmap(source_image, slice);
+   efl_gfx_buffer_update_add(source_image, NULL);
 }
+
 
 EAPI_MAIN void
 efl_main(void *data EINA_UNUSED, const Efl_Event *ev EINA_UNUSED)
@@ -80,4 +80,3 @@ efl_main(void *data EINA_UNUSED, const Efl_Event *ev EINA_UNUSED)
    _gui_setup();
 }
 EFL_MAIN()
-
